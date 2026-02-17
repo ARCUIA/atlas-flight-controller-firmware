b102221d35a3441f617b087138c0015064dfdf7a
disgruntled-frog <manleysam64@gmail.com>
2026-02-17 01:48:20 -0600

    switching to spi/i2c version for drivers to allow for switching between both easily. init commit

---
+++ flight-controller-software/lib/Core/src/IBus.hpp
@@ -0,0 +1,14 @@
+#ifndef IBUS_H
+#define IBUS_H
+
+#include <stdint.h>
+
+class IBus {
+    public:
+        virtual ~IBus() = default;
+
+        virtual bool write(uint8_t reg, uint8_t value) = 0;
+        virtual bool read(uint8_t reg, uint8_t* buffer, uint8_t length) = 0;
+};
+
+#endif
--- flight-controller-software/lib/LSM6DSV80X/LSM6DSV80.cpp
+++ flight-controller-software/lib/LSM6DSV80X/LSM6DSV80.cpp
@@ -1,1 +1,5 @@
 #include "LSM6DSV80X.h"
+
+#define WHOAMI_REG = 0x0F;
+
+LSM6DSV80X::LSM6DSV80X(SPIClass& spi, uint8_t csPin) : _spi(spi), _cs(csPin) {}
